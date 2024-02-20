a980a8fff17bb814687f42ac362aa62741e4a994
Zhoodar Kasymov <zhoodar.kasymov@gmail.com>
2024-02-20 16:06:31 +0600

    Moved method with downloading document to BranchController

--- BookingQueue/Controllers/BranchController.cs
+++ BookingQueue/Controllers/BranchController.cs
@@ -1,11 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using BookingQueue.BLL.Resources;
+using BookingQueue.Common.Enums;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookingQueue.Controllers;
 
 public class BranchController : Controller
 {
+    private readonly IWebHostEnvironment _hostEnvironment;
+    private readonly LocService _localization;
+
+    public BranchController(IWebHostEnvironment hostEnvironment, LocService localization)
+    {
+        _hostEnvironment = hostEnvironment;
+        _localization = localization;
+    }
+    
     public IActionResult Index()
     {
         return View();
     }
+    
+    public IActionResult DownloadDocs(DocTypeEnum docType)
+    {
+        var file = GetFileFromDocType(docType);
+
+        if (file is null) return NotFound();
+        
+        return file;
+    }
+
+    #region Private Methods
+
+    private FileStreamResult? GetFileFromDocType(DocTypeEnum docType)
+    {
+        var isRus = string.Equals(CultureInfo.CurrentCulture.Name, "ru", StringComparison.OrdinalIgnoreCase);
+        var webRootPath = _hostEnvironment.WebRootPath;
+        string downloadFileText;
+        string filePath;
+
+        switch (docType)
+        {
+            case DocTypeEnum.Privacy:
+                filePath = Path.Combine(webRootPath, "documents\\privacy.pdf");
+                downloadFileText = "Документ условия и соглашения.pdf";
+                break;
+            case DocTypeEnum.InstructionForQueue:
+                filePath = Path.Combine(webRootPath, isRus ? "documents\\InstructionsRus.pdf" : "documents\\InstructionKG.pdf");
+                downloadFileText = $"{_localization.GetLocalizedString("Email_request")}.pdf";
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(docType), docType, @"Документ для скачивания не найден");
+        }
+
+        return System.IO.File.Exists(filePath) 
+            ? File(System.IO.File.OpenRead(filePath), "application/pdf", downloadFileText) 
+            : null;
+    }
+
+    #endregion
 }
--- BookingQueue/Controllers/HomeController.cs
+++ BookingQueue/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System.Globalization;
-using AspNetCore.ReCaptcha;
+﻿using AspNetCore.ReCaptcha;
 using BookingQueue.BLL.Resources;
 using BookingQueue.BLL.Services.Interfaces;
-using BookingQueue.Common.Enums;
 using BookingQueue.Common.Models.ViewModels;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
@@ -13,18 +11,15 @@ namespace BookingQueue.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly IWebHostEnvironment _hostEnvironment;
     private readonly IServicesService _servicesService;
     private readonly IAdvanceService _advanceService;
     private readonly LocService _localization;
 
     public HomeController(
-        IWebHostEnvironment hostEnvironment, 
         IServicesService servicesService,
         IAdvanceService advanceService,
         LocService localization)
     {
-        _hostEnvironment = hostEnvironment;
         _servicesService = servicesService;
         _advanceService = advanceService;
         _localization = localization;
@@ -95,16 +90,6 @@ public class HomeController : Controller
 
         return LocalRedirect(returnUrl);
     }
-    
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult DownloadDocs(DocTypeEnum docType)
-    {
-        var file = GetFileFromDocType(docType);
-
-        if (file is null) return NotFound();
-        
-        return file;
-    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
@@ -119,32 +104,6 @@ public class HomeController : Controller
         if (bookingDate is null) throw new Exception(_localization.GetLocalizedString("ChooseDateAndTimePlease"));
         
         if (serviceId is null) throw new Exception(_localization.GetLocalizedString("ChooseServicesPlease"));
-    }
-
-    private FileStreamResult? GetFileFromDocType(DocTypeEnum docType)
-    {
-        var isRus = string.Equals(CultureInfo.CurrentCulture.Name, "ru", StringComparison.OrdinalIgnoreCase);
-        var webRootPath = _hostEnvironment.WebRootPath;
-        string downloadFileText;
-        string filePath;
-
-        switch (docType)
-        {
-            case DocTypeEnum.Privacy:
-                filePath = Path.Combine(webRootPath, "documents\\privacy.pdf");
-                downloadFileText = "Документ условия и соглашения.pdf";
-                break;
-            case DocTypeEnum.InstructionForQueue:
-                filePath = Path.Combine(webRootPath, isRus ? "documents\\InstructionsRus.pdf" : "documents\\InstructionKG.pdf");
-                downloadFileText = $"{_localization.GetLocalizedString("Email_request")}.pdf";
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(docType), docType, @"Документ для скачивания не найден");
-        }
-
-        return System.IO.File.Exists(filePath) 
-            ? File(System.IO.File.OpenRead(filePath), "application/pdf", downloadFileText) 
-            : null;
     }
 
     #endregion
